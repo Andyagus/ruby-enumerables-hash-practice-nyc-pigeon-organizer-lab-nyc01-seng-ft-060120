c7e746aca58b682ffe30b41233cfab43c3593dd4
Andy Agus <redagus@gmail.com>
2020-05-26 17:32:57 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,6 +1,7 @@
 require 'pry'
 
 def nyc_pigeon_organizer(data)
+<<<<<<< HEAD
   # write your code here!
   new_hash = {}
   data.each do |property, hash|
@@ -22,3 +23,11 @@ def nyc_pigeon_organizer(data)
   end
   new_hash
 end
+=======
+  data.each_with_object({}) do |(key, value), final_array|
+    value.each do |inner_key, name|
+    binding.pry
+    final_array
+  end
+end
+>>>>>>> e1a7632e3440a94acf2d4219da8653edcde3b062
